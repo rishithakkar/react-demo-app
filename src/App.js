756b43c00f0e1806edb53924a3f296593fdcdc77
rishithakkar <rishithakkar48@gmail.com>
2022-04-07 09:47:10 +0530

    init: practice app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+
+  const incre = () => {
+    setCount(count + 1);
+    console.log("incre count: ", count);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Counter</h1>
+
+      <button onClick={() => setCount(count - 2)}>-</button>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <h3>{count}</h3>
+      <button onClick={incre}>+</button>
+    </>
   );
 }
 

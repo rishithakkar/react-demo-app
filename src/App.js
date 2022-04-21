84c5ef5b25ac6261458b5d9413cbdb8bebfbdd20
rishithakkar <rishithakkar48@gmail.com>
2022-04-21 22:47:33 +0530

    clean: master branch

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import "./App.css";
-import React, { useState } from "react";
-import Button from "./components/Button";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("");
-
-  const incre = (e) => {
-    setCount(count + 1);
-  };
-
-  const decre = () => {
-    setCount(count - 1);
-  };
 
   return (
-    <div className="ml-3">
-      <h1>Counter</h1>
-
-      <Button type="submit" count={count} eventHandler={decre}>
-        -
-      </Button>
-      {/* <button onClick={() => setCount(count - 2)}>-</button> */}
-      <h3>{count}</h3>
-      <Button type="cancel" eventHandler={incre}>
-        +
-      </Button>
-      {/* <button onClick={incre}>+</button> */}
-
-      <br />
-      <input name={name} onChange={(e) => setName(e.target.value)} type="text" />
-      <br />
-      <br />
-
-      {count % 2 == 0 ? "Even" : "Odd"}
+    <div>
+      <h1>React demo application</h1>
     </div>
   );
 }
